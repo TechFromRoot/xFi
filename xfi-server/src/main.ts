2a6562d9cd8d7e1ca2e78f3d88dfd35a4dbdbf5d
eketeUg <emmanuelugochukwuekete@gmail.com>
2025-05-17 00:20:23 +0100

    user details

--- xfi-server/src/main.ts
+++ xfi-server/src/main.ts
@@ -5,6 +5,10 @@ import * as passport from 'passport';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors({
+    origin: '*',
+    credentials: true,
+  });
   app.use(
     session({
       secret: process.env.SESSION_SECRET || 'secret',
