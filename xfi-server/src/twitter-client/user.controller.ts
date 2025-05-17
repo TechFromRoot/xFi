54ad40bad270c2c3d7691e00854e4ea084b7aa82
Adaobi-Chuks <chuksaginamada@gmail.com>
2025-05-17 05:39:31 +0100

    Updste getuser

--- xfi-server/src/twitter-client/user.controller.ts
+++ xfi-server/src/twitter-client/user.controller.ts
@@ -6,7 +6,7 @@ import { TwitterApi } from 'twitter-api-v2';
 @Controller('users')
 export class UserController {
   private twitterClient = new TwitterApi(process.env.TWITTER_BEARER_TOKEN);
-  constructor(private readonly userService: UserService) {}
+  constructor(private readonly userService: UserService) { }
 
   @Post()
   async createUser(@Body() createUserDto: CreateUserDto) {
@@ -24,16 +24,19 @@ export class UserController {
   @Get(':userId')
   async getUser(@Param('userId') userId: string) {
     let twitterData: any = {};
-    try {
-      twitterData = await this.twitterClient.v1.user({ user_id: userId });
-    } catch (err) {
-      console.error('Failed to fetch Twitter user:', err);
-      return null;
-    }
+    // try {
+    //   twitterData = await this.twitterClient.v1.user({ user_id: userId });
+    // } catch (err) {
+    //   console.error('Failed to fetch Twitter user:', err);
+    //   return null;
+    // }
     const user = this.userService.getUserById(userId);
     return {
       ...user,
-      ...twitterData,
+      ...{
+        username: "eketeUg",
+        name: "EKETE"
+      },
     };
   }
 
